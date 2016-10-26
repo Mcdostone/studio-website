8648779b8c2e850d2603d8684bcb5b4a95ba8435
Mcdostone <yann.prono@telecomnancy.net>
2016-10-27 00:22:24 +0200

    Define the correct timezone

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      puts "-------------" + auth.credentials.expires_at.inspect
       user.save! if user.email.split('@')[1] == "telecomnancy.net"
-      if(user.save!)
-        puts User.all.inspect
-      end
     end
   end
 end
