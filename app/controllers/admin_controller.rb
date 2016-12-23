f82a25317e1370a27a3cdd03b7d22211cd3f86e3
Mcdostone <yann.prono@telecomnancy.net>
2016-12-23 17:19:48 +0100

    admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,12 @@ class AdminController < ApplicationController
 
 	before_action :require_author
 
+	def index
+		@media_count =  Medium.count
+		@events_count =  Event.count
+		@types_count =  Type.count
+	end
+
 	def require_admin
 		redirect_to root_path unless current_user.admin?
 		return
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get "/auth/google_oauth2", as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'auth/:provider/callback', to: 'sessions#create'
+  get 'admin/', to: 'admin#index'
   get 'auth/failure', to: redirect('/')
 
   resources :sessions, only: [:create, :destroy]
