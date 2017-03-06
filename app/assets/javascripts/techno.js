7c0da8fcc2da63c7296a7be2eddff5126289e145
Mcdostone <yann.prono@telecomnancy.net>
2017-03-06 13:47:30 +0100

    More images for countdown

--- app/assets/javascripts/techno.js
+++ app/assets/javascripts/techno.js
@@ -10,14 +10,14 @@ function onPlayerReady(event) {
   $('body').trigger('click')
 }
 
-function getRandom() {
-  return
-}
-
 function add_image(url, classes) {
   var img = document.createElement('img')
   img.src = url
-  img.classList.add('countdown-image', classes)
+  img.classList.add('countdown-image')
+  if(classes !== undefined) {
+    classes.forEach(e => img.classList.add(e))
+  }
+
   let ecart = Math.floor(Math.random() * 250 ) -  100;
   let top = Math.floor(Math.random() * window.innerHeight - 100)
   let left = Math.floor(Math.random() * window.innerWidth - 100)
@@ -48,21 +48,34 @@ function random_color() {
 
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(random_color, 21400);
-    setTimeout(boom, 9650);
-    setTimeout(() => add_image("/images/nothing5.gif"), 3000);
-    setTimeout(() => add_image("/images/loading1.png", 'rotate-lol'), 4000);
-    setTimeout(() => add_image("/images/loading2.png", 'techno'), 6000);
-    setTimeout(() => add_image("/images/loading3.png", 'techno'), 8000);
-    setTimeout(() => add_image("/images/illuminabite.gif"), 8000);
-    setTimeout(() => add_image("/images/loading4.gif", 'techno'), 2000);
-    setTimeout(() => add_image("/images/loading5.gif"), 30000);
-    setTimeout(() => add_image("/images/patou.jpg", 'techno'), 10000);
-    setTimeout(() => add_image("https://media.giphy.com/media/uudNRE0I3Ehyg/giphy.gif", 'techno'), 12000);
-    setTimeout(() => add_image("/images/loading5.png", 'techno'), 15000);
-    setTimeout(() => add_image("/images/etbanon.png", 'techno'), 15000);
+    setTimeout(random_color, 21400)
+    setTimeout(boom, 9650)
+    setTimeout(() => add_image("/images/nothing5.gif"), 3000)
+    setTimeout(() => add_image("/images/loading1.png", ['rotate-lol']), 4000)
+    setTimeout(() => add_image("/images/loading3.png", ['animated', 'rotateOut', 'infinite']), 8000)
+    setTimeout(() => add_image("/images/illuminabite.gif"), 8000)
+    setTimeout(() => add_image("/images/loading4.gif", ['techno']), 2000)
+    setTimeout(() => add_image("/images/loading5.gif"), 30000)
+    setTimeout(() => add_image("/images/patou.jpg", ['animated', 'lightSpeedIn', 'infinite']), 10000)
+    setTimeout(() => add_image("https://media.giphy.com/media/uudNRE0I3Ehyg/giphy.gif", ['techno']), 12000)
+    setTimeout(() => add_image("/images/loading5.png", ['techno']), 15000)
+    setTimeout(() => add_image("/images/etbanon.png", ['animated', 'zoomInDown', 'infinite']), 15000)
+    setTimeout(() => add_image("/images/loading6.png", ['animated', 'flipInY', 'infinite']), 6000)
+    setTimeout(() => add_image("/images/default-profile-picture.png", ['techno']), 6000)
+    setTimeout(() => add_image("/images/loading7.gif"), 8000)
+    setTimeout(() => add_image("/images/loading8.gif", ['techno']), 8000)
+    setTimeout(() => add_image("/images/loading9.jpg", ['animated', 'zoomOutLeft', 'infinite']), 12000)
+    setTimeout(() => add_image("/images/loading10.jpg", ['animated', 'fadeInLeft', 'infinite']), 13000)
+    setTimeout(() => add_image("/images/loading11.gif"), 21000)
+    setTimeout(() => add_image("/images/loading12.png", ['animated', 'flipInX', 'infinite']), 23000)
+    setTimeout(() => add_image("/images/loading13.gif", ['techno']), 22000)
+
     done = true;
   }
+
+  if(event.data == YT.PlayerState.ENDED)
+    player.seekTo(0)
+
 }
 
 $(function() {
@@ -92,6 +105,4 @@ $(function() {
       }
     })
   })
-
-
 })
